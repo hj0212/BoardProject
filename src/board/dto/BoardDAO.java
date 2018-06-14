2e004fe9d202b6fad6f330887199300955e9fa0d
SpearZero <iskcy@naver.com>
2018-06-14 15:16:13 +0900

    BoardDAO writeArticle 완성
    글쓰기 기능

--- src/board/dto/BoardDAO.java
+++ src/board/dto/BoardDAO.java
@@ -2,6 +2,7 @@ package board.dto;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class BoardDAO {
 	private Connection getConnection() throws Exception {
@@ -12,5 +13,20 @@ public class BoardDAO {
 		return DriverManager.getConnection(dbURL,dbId,dbPw);
 	}
 	
-	
+	public int writeArticle(BoardDTO dto) throws Exception{
+		Connection conn = this.getConnection();
+		String sql = "INSERT INTO BOARDDB VALUES(BOARD_SEQ.NEXTVAL,?,?,?,sysdate,0,?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, dto.getTitle());
+		pstmt.setString(2, dto.getPassword());
+		pstmt.setString(3, dto.getContents());
+		pstmt.setString(4, dto.getIp());
+		
+		int result = pstmt.executeUpdate();
+		
+		conn.commit();
+		pstmt.close();
+		conn.close();
+		return result;
+	}
 }
