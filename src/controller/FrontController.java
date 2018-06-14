e130c5b9e9aa56270d8e12b73f33268b38cf07f7
HJ Kwon <hyejin0212@gmail.com>
2018-06-14 14:59:13 +0900

    FrontController 생성

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class FrontController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("*.bo")
+public class FrontController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
 
 }
