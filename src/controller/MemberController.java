e7d681b2be6489d71a19bf0358873350c42e3c26
winhae <winhae@gmail.com>
2018-06-15 11:13:41 +0900

    페이지 네비 적용했어요!!!
    하하하하하

---
+++ src/controller/MemberController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.dto.BoardDAO;
+import member.dto.MemberDAO;
+
+
+@WebServlet("*.do")
+public class MemberController extends HttpServlet { 
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI= request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		request.setCharacterEncoding("UTF-8");
+		
+		BoardDAO bdao = new BoardDAO();
+		MemberDAO mdao = new MemberDAO();
+
+		boolean isForward = true;
+		String dst = null;
+		
+		if(command.equals("/login.do")) {
+			
+		}
+		
+		if(isForward) {
+			RequestDispatcher rd = request.getRequestDispatcher(dst);
+			rd.forward(request, response);
+		} else {
+			response.sendRedirect(dst);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
